7924b5df0738bff2cb01d2f62076a2d83daacda7
gabriel <gabrieldeolival2@gmail.com>
2026-04-04 10:56:59 -0400

    feat(fsm): :sparkles: Adding the base template for a simple FSM (State Machine)

--- common/state_machines/state_base.gd
+++ common/state_machines/state_base.gd
@@ -1,1 +1,12 @@
+class_name StateBase
 extends Node
+
+@onready var controlled_node: Node = self.owner
+var state_machine: StateMachine
+
+# --- FUNCIONES ---
+func start() -> void:
+	pass # Se inicia al empezar el estado
+
+func exit() -> void:
+	pass # Se ejecuta al cambiar el estado
