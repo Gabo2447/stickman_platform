d188497dc6c281bc40f82555478dca7be8706578
gabriel <gabrieldeolival2@gmail.com>
2026-04-04 18:52:17 -0400

    feat(player): :sparkles: adding mechanic jumo in player fsm

--- scenes/entities/player/states/player_state_jump.gd
+++ scenes/entities/player/states/player_state_jump.gd
@@ -1,4 +1,17 @@
 extends PlayerStateBase
 
 func start() -> void:
-	print(ANIM_JUMP)
+	player.animation_player.play(ANIM_JUMP)
+	player.velocity.y = stats.jump_force
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_axis("left", "right")
+
+	player.velocity.y += handle_gravity(delta)
+	player.velocity.x = handle_movement_horizontal(direction)
+
+	if player.velocity.x != 0:
+		player.sprite.flip_h = is_flip_sprite(player.velocity.x)
+	
+	if player.velocity.y < 0:
+		state_machine.change_to(STATE_FALL)
